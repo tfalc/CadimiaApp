b011e49717ea7b58f57cadc93236dfb668054b2a
Thiago F <thiago.falcao86@gmail.com>
2023-02-22 13:28:16 -0300

    feat: Creation of utils classes

---
+++ src/main/java/com/tfalc/gymapp/utils/LocalDateDeserializer.java
@@ -0,0 +1,22 @@
+package com.tfalc.gymapp.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+
+    private static final long serialVersionUID = -4589164298310392227L;
+
+    public LocalDateDeserializer() {
+        super(LocalDate.class);
+    }
+
+    @Override
+    public LocalDate deserialize(final JsonParser parser, final DeserializationContext context) throws IOException {
+        return LocalDate.parse(parser.readValueAs(String.class), JavaTimeUtil.LOCAL_DATE_FORMATTER);
+    }
+}
